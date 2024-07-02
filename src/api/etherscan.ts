89e5e1e79975a084cddcf1446eeb1baab3d6c867
Toni-d-e-v <toni.dumancic3@gmail.com>
2024-07-02 13:27:55 +0200

    Add erc20 support

--- src/api/etherscan.ts
+++ src/api/etherscan.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 let ETHERSCAN_API_KEY = 'YAWP8ZXZBG8YWBFW2T7TSFRFIT41D8FHEE'; // Replace with your Etherscan API key
 let ETHERSCAN_API_BASE_URL = 'https://api.etherscan.io/api';
 
-export const getTransactionHistory = async (address: string, custom_api_key?: string, custom_api_url?: string) => {
+export const getTransactionHistory1 = async (address: string, custom_api_key?: string, custom_api_url?: string) => {
   if (custom_api_key) {
     ETHERSCAN_API_KEY = custom_api_key;
   }
@@ -21,5 +21,42 @@ export const getTransactionHistory = async (address: string, custom_api_key?: st
   } catch (error) {
     console.error('Error fetching transaction history:', error);
     return [];
+  }
+};
+
+
+export const getTransactionHistory = async (address: string, custom_api_key?: string, custom_api_url?: string) => {
+  if (custom_api_key) {
+    ETHERSCAN_API_KEY = custom_api_key;
+  }
+  if (custom_api_url) {
+    ETHERSCAN_API_BASE_URL = custom_api_url;
+  }
+
+  try {
+    const [ethTxResponse, erc20TxResponse, erc721TxResponse] = await Promise.all([
+      axios.get(`${ETHERSCAN_API_BASE_URL}?module=account&action=txlist&address=${address}&apikey=${ETHERSCAN_API_KEY}`),
+      axios.get(`${ETHERSCAN_API_BASE_URL}?module=account&action=tokentx&address=${address}&apikey=${ETHERSCAN_API_KEY}`),
+      axios.get(`${ETHERSCAN_API_BASE_URL}?module=account&action=tokennfttx&address=${address}&apikey=${ETHERSCAN_API_KEY}`)
+    ]);
+
+    if (ethTxResponse.data.status === '0') {
+      throw new Error(ethTxResponse.data.result);
+    }
+    if (erc20TxResponse.data.status === '0') {
+      throw new Error(erc20TxResponse.data.result);
+    }
+
+
+    return {
+      ethTransactions: ethTxResponse.data.result,
+      erc20Transactions: erc20TxResponse.data.result,
+    };
+  } catch (error) {
+    console.error('Error fetching transaction history:', error);
+    return {
+      ethTransactions: [],
+      erc20Transactions: [],
+    };
   }
 };
--- src/components/TransactionHistory.tsx
+++ src/components/TransactionHistory.tsx
@@ -1,7 +1,5 @@
-import React from 'react';
-
-import { useState } from 'react';
-import ReactFlow, { Background, Controls,Edge, Node,XYPosition,MarkerType  } from 'reactflow';
+import React, { useState } from 'react';
+import ReactFlow, { Background, Controls, Edge, Node, XYPosition, MarkerType } from 'reactflow';
 import 'reactflow/dist/style.css';
 import '../css/Popup.css'; // Create a CSS file to style the popup
 
@@ -11,56 +9,49 @@ interface PopupProps {
   explorer_url: string;
 }
 
-const Popup: React.FC<PopupProps> = ({ nodeData, onClose,explorer_url }) => {
+const Popup: React.FC<PopupProps> = ({ nodeData, onClose, explorer_url }) => {
   let nodeType = '';
   if (nodeData.id.startsWith('transaction')) {
     nodeType = 'Transaction';
   } else if (nodeData.id.startsWith('address')) {
     nodeType = 'Address';
-  } 
-  else {
+  } else {
     nodeType = 'Unknown';
   }
+
   return (
     <div className="popup-container">
       {/* Popup content */}
       <div className="popup">
         <h3>Details</h3>
-        {
-          nodeType === 'Transaction' && (
-            <div>
-              <p>Transaction Hash: {nodeData.id.substring(12).substring(0, 24)}</p>
-              <p>Transaction Value: {nodeData.data.label.substring(14)}</p>
-              <a style={{ color: 'black' }} href={`${explorer_url}tx/${nodeData.id.substring(12)}`} target="_blank" rel="noopener noreferrer">View on Etherscan</a>
+        {nodeType === 'Transaction' && (
+          <div>
+            <p>Transaction Hash: {nodeData.id.substring(12).substring(0, 24)}</p>
+            <p>Transaction Value: {nodeData.data.label.substring(14)}</p>
+            <a style={{ color: 'black' }} href={`${explorer_url}tx/${nodeData.id.substring(12)}`} target="_blank" rel="noopener noreferrer">
+              View on Etherscan
+            </a>
+          </div>
+        )}
+        {nodeType === 'Address' && (
+          <div>
+            <p>Address: {nodeData.id.substring(8)}</p>
+            <div style={{ display: 'flex', padding: '1px' }}>
+              <a style={{ color: 'black', margin: '4px' }} href={`${explorer_url}/address/${nodeData.id.substring(8)}`} target="_blank" rel="noopener noreferrer">
+                View on Etherscan
+              </a>
+              {/* <a style={{ color: 'black', margin: '4px' }} href={`/?address=${nodeData.id.substring(8)}`} target="_blank" rel="noopener noreferrer">Open in EVMSpect</a> */}
             </div>
-          )
-        }
-        {
-          nodeType === 'Address' && (
-            <div>
-              <p>Address: {nodeData.id.substring(8)}</p>
-              <div style={{ display: 'flex', padding: '1px'}}>
-                  
-                <a style={{ color: 'black', margin: '4px'}} href={`${explorer_url}/address/${nodeData.id.substring(8)}`} target="_blank" rel="noopener noreferrer">View on Etherscan</a>
-
-                {/* <a style={{ color: 'black', margin: '4px' }} href={`/?address=${nodeData.id.substring(8)}`} target="_blank" rel="noopener noreferrer">Open in EVMSpect</a> */}
-              </div>
-            </div>
-          )
-      }
-      {
-          nodeType === 'Unknown' && (
-            <div>
-              <p>
-                {nodeData.id}
-              </p>
-              <a style={{ color: 'black', margin: '4px'}} href={`${explorer_url}/address/${nodeData.id}`} target="_blank" rel="noopener noreferrer">View on Etherscan</a>
-
-            </div>
-          )
-      }
-         <p></p>
-        {/* Add other node details you want to display */}
+          </div>
+        )}
+        {nodeType === 'Unknown' && (
+          <div>
+            <p>{nodeData.id}</p>
+            <a style={{ color: 'black', margin: '4px' }} href={`${explorer_url}/address/${nodeData.id}`} target="_blank" rel="noopener noreferrer">
+              View on Etherscan
+            </a>
+          </div>
+        )}
         <button onClick={onClose}>Close</button>
       </div>
     </div>
@@ -86,6 +77,8 @@ interface EVMtx {
   cumulativeGasUsed: string;
   gasUsed: string;
   confirmations: string;
+  tokenDecimal: number;
+  tokenSymbol: string;
 }
 
 interface TransactionHistoryProps {
@@ -95,26 +88,35 @@ interface TransactionHistoryProps {
   explorer_url: string;
 }
 
-const TransactionHistory: React.FC<TransactionHistoryProps> = ({ transactions, our_address, related_addresses,explorer_url }) => {
+const TransactionHistory: React.FC<TransactionHistoryProps> = ({ transactions, our_address, related_addresses, explorer_url }) => {
   const [selectedNode, setSelectedNode] = useState<Node | null>(null);
+  const [filteredNodes, setFilteredNodes] = useState<Node[]>([]);
+  const [filteredEdges, setFilteredEdges] = useState<Edge[]>([]);
+  const [isFiltering, setIsFiltering] = useState(false);
+
   if (explorer_url === '') {
     explorer_url = 'https://etherscan.io/';
   }
-  // Function to handle element click
+
   const handleElementClick = (event: React.MouseEvent, element: any) => {
     if (element?.id && element?.type === 'default') {
       setSelectedNode(element);
+      applyFilter(element);
     } else {
       setSelectedNode(null);
+      setIsFiltering(false);
     }
   };
+
   const closePopup = () => {
     setSelectedNode(null);
+    setIsFiltering(false);
   };
 
   if (!our_address) {
     return null;
   }
+
   const ourNode = {
     id: our_address,
     type: 'default',
@@ -122,76 +124,85 @@ const TransactionHistory: React.FC<TransactionHistoryProps> = ({ transactions, o
     data: { label: our_address.substring(0, 12) },
   };
 
-  // Create nodes for related addresses
   const relatedNodes: Node[] = related_addresses.map((address, index) => ({
     id: address,
     type: 'default',
     position: {
-      x: 200, // Adjust the position as needed to place the nodes on the right side
-      y: 100 * index, // Adjust the spacing between nodes on the right side
+      x: 200,
+      y: 100 * index,
     },
     data: { label: address.substring(0, 12) },
   }));
 
-  // Create nodes for transactions that send to our_address
   const transactionNodesToUs: Node[] = transactions
     .filter((transaction) => transaction.to.toLowerCase() === our_address.toLowerCase())
-    .map((transaction, index) => ({
-      id: `transaction-${transaction.hash}`,
-      type: 'default',
-      position: {
-        x: -400, // Place transactions on the right side, adjust as needed
-        y: 100 * (related_addresses.length + index), // Adjust the vertical spacing between transaction nodes
-      },
-      data: { label: `Transaction ${transaction.hash.substring(0, 12)} - ${Number(transaction.value) / 1000000000000000000} ETH` },
-    }));
+    .map((transaction, index) => {
+      const value = transaction.tokenSymbol
+        ? `${Number(transaction.value) / Math.pow(10, transaction.tokenDecimal)} ${transaction.tokenSymbol}`
+        : `${Number(transaction.value) / 1000000000000000000} ETH`;
+
+      return {
+        id: `transaction-${transaction.hash}`,
+        type: 'default',
+        position: {
+          x: -400,
+          y: 100 * (related_addresses.length + index),
+        },
+        data: { label: `Transaction ${transaction.hash.substring(0, 12)} - ${value}` },
+      };
+    });
+
   const transactionNodesFromUs: Node[] = transactions
     .filter((transaction) => transaction.from.toLowerCase() === our_address.toLowerCase())
-    .map((transaction, index) => ({
-      id: `transaction-${transaction.hash}`,
-      type: 'default',
-      position: {
-        x: 600, // Place transactions on the right side, adjust as needed
-        y: 100 * (related_addresses.length + index), // Adjust the vertical spacing between transaction nodes
-      },
-      data: { label: `Transaction ${transaction.hash.substring(0, 12)} - ${Number(transaction.value) / 1000000000000000000} ETH` },
-    }));
+    .map((transaction, index) => {
+      const value = transaction.tokenSymbol
+        ? `${Number(transaction.value) / Math.pow(10, transaction.tokenDecimal)} ${transaction.tokenSymbol}`
+        : `${Number(transaction.value) / 1000000000000000000} ETH`;
+
+      return {
+        id: `transaction-${transaction.hash}`,
+        type: 'default',
+        position: {
+          x: 600,
+          y: 100 * (related_addresses.length + index),
+        },
+        data: { label: `Transaction ${transaction.hash.substring(0, 12)} - ${value}` },
+      };
+    });
+
   const relatedAddressesNodes = transactions
-    .filter((transaction) => transaction.from !== our_address.toLowerCase() )
+    .filter((transaction) => transaction.from !== our_address.toLowerCase())
     .map((transaction, index) => ({
       id: `address-${transaction.from}`,
       type: 'default',
       position: {
-        x: -800, // Place transactions on the right side, adjust as needed
-        y: 100 * (related_addresses.length + index), // Adjust the vertical spacing between transaction nodes
+        x: -800,
+        y: 100 * (related_addresses.length + index),
       },
       data: { label: `Address ${transaction.from.substring(0, 12)}` },
     }));
+
   const relatedAddressesNodes2 = transactions
-    .filter((transaction) => transaction.to !== our_address.toLowerCase() )
+    .filter((transaction) => transaction.to !== our_address.toLowerCase())
     .map((transaction, index) => ({
       id: `address-${transaction.to}`,
       type: 'default',
       position: {
-        x: 900, // Place transactions on the right side, adjust as needed
-        y: 100 * (related_addresses.length + index), // Adjust the vertical spacing between transaction nodes
+        x: 900,
+        y: 100 * (related_addresses.length + index),
       },
       data: { label: `Address ${transaction.to.substring(0, 12)}` },
     }));
 
   const tx_length = transactions.length;
-  // Create edges from related addresses to our_address
-  // Create edges from related addresses to our_address
+
   const relatedEdges: Edge[] = relatedNodes.map((node) => ({
     id: `edge-${node.id}-${our_address}`,
     source: node.id,
     target: our_address,
-    // type: 'simplebezier',
-    // if size over 1000 disable for performance
     type: tx_length > 1000 ? 'straight' : 'simplebezier',
   }));
 
-  // Create edges from our_address to transactions sent from our_address
   const relatedEdges2: Edge[] = transactions
     .filter((transaction) => transaction.to.toLowerCase() === our_address.toLowerCase())
     .map((transaction) => ({
@@ -201,7 +212,6 @@ const TransactionHistory: React.FC<TransactionHistoryProps> = ({ transactions, o
       type: tx_length > 1000 ? 'straight' : 'simplebezier',
     }));
 
-  // Create edges from our_address to transactions received by our_address
   const relatedEdges3: Edge[] = transactions
     .filter((transaction) => transaction.from.toLowerCase() === our_address.toLowerCase())
     .map((transaction) => ({
@@ -211,7 +221,6 @@ const TransactionHistory: React.FC<TransactionHistoryProps> = ({ transactions, o
       type: tx_length > 1000 ? 'straight' : 'simplebezier',
     }));
 
-  // Create edges from transactions sent from our_address to the recipient addresses
   const relatedEdges4: Edge[] = transactions
     .filter((transaction) => transaction.from.toLowerCase() === our_address.toLowerCase())
     .map((transaction) => ({
@@ -221,13 +230,13 @@ const TransactionHistory: React.FC<TransactionHistoryProps> = ({ transactions, o
       type: tx_length > 1000 ? 'straight' : 'simplebezier',
     }));
 
-  // Create edges from sender addresses to transactions that were sent to our_address
   const relatedEdges5: Edge[] = transactions.map((transaction) => ({
     id: `edge-${transaction.from}-${transaction.hash}`,
     source: `transaction-${transaction.hash}`,
     target: `address-${transaction.from}`,
     type: tx_length > 1000 ? 'straight' : 'simplebezier',
   }));
+
   const relatedEdges6: Edge[] = transactions.map((transaction) => ({
     id: `edge-${transaction.to}-${transaction.hash}`,
     source: `transaction-${transaction.hash}`,
@@ -241,24 +250,76 @@ const TransactionHistory: React.FC<TransactionHistoryProps> = ({ transactions, o
       orient: 'auto-start-reverse',
     },
   }));
+
+  const allNodes = [ourNode, ...relatedNodes, ...transactionNodesToUs, ...transactionNodesFromUs, ...relatedAddressesNodes, ...relatedAddressesNodes2];
+  const allEdges = relatedEdges.concat(relatedEdges2).concat(relatedEdges3).concat(relatedEdges4).concat(relatedEdges5).concat(relatedEdges6);
+
+  const applyFilter = (element: Node) => {
+    setIsFiltering(true);
   
-
-
-
-
-
+    if (element.id.startsWith('transaction')) {
+      // If the selected element is a transaction, show only its path
+      const transactionId = element.id;
+      
+      // Find edges related to this transaction
+      const relatedEdges = allEdges.filter(edge => edge.source === transactionId || edge.target === transactionId);
+      
+      // Find nodes related to this transaction
+      const relatedNodeIds = relatedEdges.map(edge => (edge.source === transactionId ? edge.target : edge.source));
+      
+      // Include the transaction node itself
+      const filteredNodes = allNodes.filter(node => node.id === transactionId || relatedNodeIds.includes(node.id));
+      
+      // Include the edges connected to the transaction
+      const filteredEdges = allEdges.filter(edge => filteredNodes.some(node => node.id === edge.source) && filteredNodes.some(node => node.id === edge.target));
+      
+      setFilteredNodes(filteredNodes);
+      setFilteredEdges(filteredEdges);
+    } else {
+      // Otherwise, show the main address and related nodes as before
+      const filterRelatedNodesAndEdges = (nodeId: string) => {
+        // Find directly related edges
+        const directlyRelatedEdges = allEdges.filter(edge => edge.source === nodeId || edge.target === nodeId);
+        
+        // Find nodes directly related to the selected node
+        const directlyRelatedNodeIds = directlyRelatedEdges.map(edge => (edge.source === nodeId ? edge.target : edge.source));
+        
+        // Include start and end nodes (addresses) related to the selected transactions
+        const startEndNodeIds = new Set();
+        directlyRelatedNodeIds.forEach(relatedNodeId => {
+          allEdges.forEach(edge => {
+            if (edge.source === relatedNodeId || edge.target === relatedNodeId) {
+              startEndNodeIds.add(edge.source);
+              startEndNodeIds.add(edge.target);
+            }
+          });
+        });
+        
+        // Convert Set to Array and filter nodes
+        const startEndNodeArray = Array.from(startEndNodeIds);
+        const filteredNodes = allNodes.filter(node => node.id === our_address || startEndNodeArray.includes(node.id));
+        
+        // Include edges connecting the main address node and related nodes
+        const filteredEdges = allEdges.filter(edge => filteredNodes.some(node => node.id === edge.source) && filteredNodes.some(node => node.id === edge.target));
+        
+        return { filteredNodes, filteredEdges };
+      };
+      
+      const { filteredNodes, filteredEdges } = filterRelatedNodesAndEdges(element.id);
+      setFilteredNodes(filteredNodes);
+      setFilteredEdges(filteredEdges);
+    }
+  };
+  
+  
 
   return (
     <div style={{ width: '100%', height: '100%' }}>
-      {selectedNode && (
-        <Popup nodeData={selectedNode} onClose={closePopup} explorer_url={explorer_url} />
-      )}
+      {selectedNode && <Popup nodeData={selectedNode} onClose={closePopup} explorer_url={explorer_url} />}
       <ReactFlow
-        nodes={[ourNode, ...relatedNodes, ...transactionNodesToUs, ...transactionNodesFromUs, ...relatedAddressesNodes, ...relatedAddressesNodes2]}
-        edges={relatedEdges.concat(relatedEdges2).concat(relatedEdges3).concat(relatedEdges4).concat(relatedEdges5).concat(relatedEdges6)}
-        onNodeClick={handleElementClick} // Add this line to handle the element click event
-        // snapToGrid={true}
-        // snapGrid={[20, 20]}
+        nodes={isFiltering ? filteredNodes : allNodes}
+        edges={isFiltering ? filteredEdges : allEdges}
+        onNodeClick={handleElementClick}
       >
         <Background color="#00ADB5" gap={16} />
         <Controls />
@@ -266,6 +327,5 @@ const TransactionHistory: React.FC<TransactionHistoryProps> = ({ transactions, o
     </div>
   );
 };
-
 
 export default TransactionHistory;
